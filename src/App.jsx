00c8c9d4348ecb7dc7cae48c82a66fd8889adb66
Pritam <pritamdas5370@gmail.com>
2024-10-30 14:25:33 +0530

    4th

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
     <div className="h-screen w-full">
       <Scene click={click} />
       <div className="h-full w-full absolute top-0 left-0 pointer-events-none">
-        <div className=" flex gap-5 absolute left-1/2 -translate-x-1/2 bottom-14">
+        <div className=" flex gap-5 absolute left-1/2 -translate-x-1/2 bottom-14 justify-center items-center flex-wrap">
           <button onClick={()=>setclick("chair1")} className=" pointer-events-auto text-white bg-yellow-900 px-6 py-2 rounded-full text-xs sm:text-xl">Chair1</button>
           <button onClick={()=>setclick("chair2")} className=" pointer-events-auto text-white bg-yellow-900 px-6 py-2 rounded-full text-xs sm:text-xl">Chair2</button>
           <button onClick={()=>setclick("chair3")} className=" pointer-events-auto text-white bg-yellow-900 px-6 py-2 rounded-full text-xs sm:text-xl">Chair3</button>
