3d46166c15ed02c275b5ea3ff5b4d37cbc75118d
Pritam <pritamdas5370@gmail.com>
2024-10-30 15:02:14 +0530

    7th

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,5 +1,5 @@
 import { Canvas } from "@react-three/fiber";
-import React from "react";
+import React, { Suspense } from "react";
 import Experience from "./Experience";
 import {
   Billboard,
@@ -11,14 +11,11 @@ import {
 const Scene = ({ click }) => {
   return (
     <Canvas>
-      <Experience click={click} />
+      <Suspense>
+        <Experience click={click} />
+      </Suspense>
       <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} />
-        <ContactShadows
-          position-y={-1.5}
-          blur={1.5}
-          opacity={0.55}
-          scale={10}
-        />
+      <ContactShadows position-y={-1.5} blur={1.5} opacity={0.55} scale={10} />
       <Environment preset="forest" background backgroundBlurriness={100} />
     </Canvas>
   );
